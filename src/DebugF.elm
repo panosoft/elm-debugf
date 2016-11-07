863961ac36c964304c58f1d45af34083a862c164
Charles Scalfani <charles@panosoft.com>
2016-11-07 08:58:02 -0800

    Added required docs

--- src/DebugF.elm
+++ src/DebugF.elm
@@ -1,13 +1,23 @@
 module DebugF exposing (..)
 
+{-|
+    More readable debugging.
+
+@docs toStringF, log
+-}
+
 import Native.DebugF
 
 
+{-| more readable toString
+-}
 toStringF : a -> String
 toStringF =
     Native.DebugF.toStringF
 
 
+{-| more readable log
+-}
 log : String -> a -> a
 log =
     Native.DebugF.log
